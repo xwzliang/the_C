f01a0c7e46f80e05c8b4bc3b18b86ab4bfe94638
broliang <18706703437@163.com>
2019-09-09 21:38:26 +0800

    Refactor test.c: Add blank lines to improve readability

--- 01_a_tutorial_introduction/test.c
+++ 01_a_tutorial_introduction/test.c
@@ -6,17 +6,24 @@
 void test_hello_world(void) {
 	int stdout_bk; 	// is fd for stdout backup
 	stdout_bk = dup(fileno(stdout));
+
 	int pipefd[2];
 	pipe(pipefd);
+
 	// What used to be stdout will now go to the pipe.
 	dup2(pipefd[1], fileno(stdout));
+
 	hello_world();
+
 	fflush(stdout);		//flushall();
 	write(pipefd[1], "", 9); 	// null-terminated string!
 	close(pipefd[1]);
+
 	dup2(stdout_bk, fileno(stdout));	//restore
+
 	char buf[101];
 	read(pipefd[0], buf, 100);
+
 	TEST_ASSERT_EQUAL_STRING(buf, "hello, world\n");
 }
 
