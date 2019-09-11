6a518dcdef116ed7771ca875206132d6cae0aaad
broliang <18706703437@163.com>
2019-09-11 20:10:53 +0800

    Modify 10_power.c: reduce argument n in power function to test arguments are passed 'by value' in C, test still passes

--- 01_a_tutorial_introduction/10_power.c
+++ 01_a_tutorial_introduction/10_power.c
@@ -1,9 +1,8 @@
 /*power: raise base to n-th power; n >= 0*/
 int power(int base, int n) {
-	int i, p;
+	int p;
 
-	p = 1;
-	for (i = 1; i <= n; ++i)
+	for (p = 1; n > 0; --n)
 		p = p * base;
 	return p;
 }
