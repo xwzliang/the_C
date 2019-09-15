5f77709af6f555210bb1b6374ae4dba8ef51566b
broliang <18706703437@163.com>
2019-09-15 16:28:43 +0800

    Add 03_str_len.c: Add another vertion of str_len using address arithmetic, test still passed

--- 03_pointers_and_arrays/03_str_len.c
+++ 03_pointers_and_arrays/03_str_len.c
@@ -1,8 +1,16 @@
 /*str_len: return length of string s*/
-int str_len(char *s) {	// change name to avoid conflict with built-in strlen
-	int n;
+/*int str_len(char *s) {	// change name to avoid conflict with built-in strlen*/
+	/*int n;*/
 
-	for (n = 0; *s != '\0'; s++)
-		n++;
-	return n;
+	/*for (n = 0; *s != '\0'; s++)*/
+		/*n++;*/
+	/*return n;*/
+/*}*/
+
+int str_len(char *s) {
+	char *p = s;
+
+	while (*p != '\0')
+		p++;
+	return p - s;
 }
