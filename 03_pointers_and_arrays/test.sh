ab2ae2916452142ad2c7ae38f0d8e43a52cc047f
broliang <18706703437@163.com>
2019-09-15 21:42:26 +0800

    New echo and add test for it, test passed

--- 03_pointers_and_arrays/test.sh
+++ 03_pointers_and_arrays/test.sh
@@ -9,3 +9,20 @@ gcc -I $unity_path $unity_path/unity.c *.c ../02_functions_and_program_structure
 	01234
 	Hello, world.
 _EOF_
+
+# Test for echo
+cd 09_command_line_arguments
+gcc echo.c -o echo.out
+chmod +x ./echo.out
+output=$(./echo.out "Hello," "world.")
+expect="Hello, world."
+if [ "$output" == "$expect" ]; then
+	echo "test for echo passed."
+else
+	echo "test for echo failed."
+	echo "Got:"Expect 
+	echo "$output" | cat -A
+	echo "Expect:"
+	echo "$expect" | cat -A
+fi
+
